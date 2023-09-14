871fbe61d638f5019e7c7e3a3ce1a1baaa8e2d73
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-09-15 02:03:30 +0600

    Completed!

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -4,7 +4,7 @@
 const Header = () => {
     return (
         <div>
-            <h1 className="text-center text-3xl text-gray-700 md:text-3xl font-semibold py-[20px] md:py-[45px]">Course Registration</h1>
+            <h1 className="text-center text-3xl text-gray-700 md:text-3xl font-semibold pb-[20px] md:pb-[45px]">Course Registration</h1>
         </div>
     );
 };
